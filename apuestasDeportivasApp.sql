578410253ae8f32e459b2d936032f1a5e3e483ec
roocioseoane <roocioseoane22@gmail.com>
2022-05-04 15:41:16 +0200

    Insertamos datos
    Insertamos datos para verificar que la base de datos funciona correctamente.

--- apuestasDeportivasApp.sql
+++ apuestasDeportivasApp.sql
@@ -63,3 +63,61 @@ create table apuestas(
 	id_TransaccionC int references transacciones(id_transaccion),
 	id_TransaccionP int references transacciones(id_transaccion)
 )
+
+/***Insertamos datos***/
+INSERT INTO tipoTransacciones(nombre, descripcion) VALUES ('Ingreso', 'Ingreso externo')
+INSERT INTO tipoTransacciones(nombre, descripcion) VALUES ('Retirada', 'Retirada externa')
+INSERT INTO tipoTransacciones(nombre, descripcion) VALUES ('Apuesta realizada', 'Retirada interna')
+INSERT INTO tipoTransacciones(nombre, descripcion) VALUES ('Apuesta ganada', 'Ingreso interno')
+
+select * from tipoTransacciones
+
+INSERT INTO usuarios(nombre, clave, saldo) VALUES ('Pepe', '142536pepe', 400.8)
+INSERT INTO usuarios(nombre, clave, saldo) VALUES ('María', '875123maria', 30)
+
+select * from usuarios
+
+INSERT INTO tipoEventos(nombre, descripcion) VALUES ('Tenis', 'Semifinales')
+INSERT INTO tipoEventos(nombre) VALUES ('F1')
+
+select * from tipoEventos
+
+INSERT INTO eventos(nombre, fecha, id_tipoEvento) VALUES ('Albert Park', '2022-04-10T07:00:00', 2)
+INSERT INTO eventos(nombre, fecha, id_tipoEvento) VALUES ('Wimbledon Londres', '2022-07-21T18:00:00', 1)
+
+select * from eventos
+
+INSERT INTO transacciones(fecha, saldo_inicial, cantidad, id_usuario, id_tipoTransaccion) VALUES ('2022-04-05T19:51:36', 400.8, 25, 1, 2)
+INSERT INTO transacciones(fecha, saldo_inicial, cantidad, id_usuario, id_tipoTransaccion) VALUES ('2022-04-09T00:05:36', 30, 25, 2, 3)
+INSERT INTO transacciones(fecha, saldo_inicial, cantidad, id_usuario, id_tipoTransaccion) VALUES ('2022-04-10T18:00:32', 5, 300, 2, 4)
+
+select * from transacciones
+
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Leclerc', 2, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Sainz', 3, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Verstappen', 4, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Russell', 5, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Hamilton', 6, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Ocon', 7, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Magnussen', 8, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Prez', 8, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Bottas', 9, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Norris', 10, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Tsunoda', 11, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Gasly', 11, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Alonso', 12, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Zhou', 13, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Schumacher', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Stroll', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Albon', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Ricciardo', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Latifi', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Shulkenberg', 14, 1)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Nadal', 7, 2)
+INSERT INTO opciones(nombre, multiplicador, id_evento) VALUES ('Swiatek', 2, 2)
+
+select * from opciones
+
+INSERT INTO apuestas(fecha, cantidad, multiplicador, id_usuario, id_opcion, id_TransaccionC, id_TransaccionP) VALUES ('2022-04-09T00:05:38', 25, 12, 2, 13, 2, 3)
+
+select * from apuestas
