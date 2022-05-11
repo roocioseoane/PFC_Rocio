a02ea026294bf866f7e1d7c99dd3a8b67710fc78
roocioseoane <roocioseoane22@gmail.com>
2022-05-11 15:36:29 +0200

    Crear Sentencias
    Cree la clase para definir las sentencias y ejecutar los comandos necesarios.

---
+++ ApuestasDeportivasApp/ApuestasDeportivasApp/Sentencias.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nombrespacio
+{
+    class Sentencias
+    {
+        
+    }
+}
