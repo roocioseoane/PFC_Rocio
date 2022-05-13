821a7c2f6aa36d86f588e2c21a628fa00d318384
roocioseoane <roocioseoane22@gmail.com>
2022-05-13 10:41:31 +0200

    Método para conseguir el id
    Creamos un método para conseguir el id del usuario cuando se registre. Y dependiendo de la opción, hace una de las siguientes opciones.

--- ApuestasDeportivasApp/ApuestasDeportivasApp/Program.cs
+++ ApuestasDeportivasApp/ApuestasDeportivasApp/Program.cs
@@ -4,8 +4,10 @@ using nombrespacio;
 
 Conexion conexion = new Conexion("localhost", "apuestasDeportivasApp", "RocioSeoane", "AbCdEf84");
 Usuarios usuarios = new Usuarios();
+Sentencias sentencias = new Sentencias();
 Admin admin = new Admin();
 
+bool salirDelBucle = false;
 int _opcion, id_usuario = -1;
 String opcion;
 
@@ -23,4 +25,38 @@ int menu()
     } while (_opcion < 0 || _opcion > 2);
 
     return _opcion;
+}
+
+int conseguirId()
+{
+    while (!salirDelBucle)
+    {
+        _opcion = menu();
+
+        switch (_opcion)
+        {
+            case 0:
+                salirDelBucle = true;
+                break;
+            case 1:
+                if((id_usuario = sentencias.logearUsuario()) != -1)
+                {
+                    salirDelBucle = true;
+                }
+                break;
+            case 2:
+                if (sentencias.registrarUsuario() == 0)
+                {
+                    Console.WriteLine("Registrado correctamente");
+                    salirDelBucle = false;
+                }
+                else
+                    Console.WriteLine("No se ha podido registrar");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+    return id_usuario;
 }
