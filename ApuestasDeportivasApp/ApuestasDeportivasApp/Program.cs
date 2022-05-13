2dd7594ec617434e43894260e3b5d00458f79746
roocioseoane <roocioseoane22@gmail.com>
2022-05-13 10:34:15 +0200

    Creamos el menú inicial
    Creamos el menú para registrarse o logearse.

--- ApuestasDeportivasApp/ApuestasDeportivasApp/Program.cs
+++ ApuestasDeportivasApp/ApuestasDeportivasApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using nombrespacio;
+
+Conexion conexion = new Conexion("localhost", "apuestasDeportivasApp", "RocioSeoane", "AbCdEf84");
+Usuarios usuarios = new Usuarios();
+Admin admin = new Admin();
+
+int _opcion, id_usuario = -1;
+String opcion;
+
+int menu()
+{
+    Console.WriteLine("OPCIONES DE LA APLICACIÓN");
+    Console.WriteLine("\n_________________________");
+    Console.WriteLine("\n0.\tSalir");
+    Console.WriteLine("\n1.\tRegistrarse");
+    Console.WriteLine("\n2.\tLogearse");
+    do
+    {
+        opcion = Console.ReadLine();
+        _opcion = Convert.ToInt32(opcion);
+    } while (_opcion < 0 || _opcion > 2);
+
+    return _opcion;
+}
--- ApuestasDeportivasApp/ApuestasDeportivasApp/Usuarios.cs
+++ ApuestasDeportivasApp/ApuestasDeportivasApp/Usuarios.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace apuestas
+namespace nombrespacio
 {
     class Usuarios
     {
